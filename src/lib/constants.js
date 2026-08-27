c988911bc9e406b0261b36d48cc8a16873c1450a
isabellaronnanderson-png <isabellaronnanderson@gmail.com>
2026-08-27 10:55:11 +0100

    Add files via upload

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -7,7 +7,7 @@ export const CATEGORIES = ['Tops', 'Bottoms', 'Dresses', 'Outerwear', 'Shoes', '
 // given this time (dark brown, red, light blue, butter yellow), so "olive"
 // reuses the same value as "blue" rather than introducing an off-palette color.
 export const PALETTE = {
-  red: '#E8623F',
+  red: '#DC583A',
   brown: '#3D2620',
   blue: '#AFC8F5',
   olive: '#AFC8F5',
